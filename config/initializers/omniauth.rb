f6f2255c266fbf810e19a56f93194ff459d824ac
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-04 14:45:49 -0600

    coding and coding

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "430049389600-ekvorou4klubrb0pf7hte30vppcv0lkk.apps.googleusercontent.com", "yMLvOZFZE8AZ03y69gg2L4Dt"
+end
